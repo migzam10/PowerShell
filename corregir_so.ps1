99d38e4046493c64d621aaf87a82387b5b5c8d99
MigZam <mz39729@gmail.com>
2026-04-13 08:54:52 -0500

    Update corregir_so.ps1
    Se cambio los scripts de desfragmentar segun el disco y el ciclo completo de DISM

--- corregir_so.ps1
+++ corregir_so.ps1
@@ -2,7 +2,7 @@ function Mostrar-Menu {
     Write-Host "Selecciona una opción:"
     Write-Host "1. Desfragmentar unidad C"
     Write-Host "2. Corregir errores del Sistema con SFC /scannow"
-	Write-Host "3. Corregir errores del Sistema con DISM /RestoreHealth" 
+	Write-Host "3. Corregir errores del Sistema con DISM" 
     Write-Host "4. Salir"
 }
 
@@ -12,25 +12,39 @@ do {
     
     switch ($opcion) {
         1 {
-            $drive = Get-CimInstance -ClassName Win32_DiskDrive | Where-Object { $_.MediaType -eq 'Fixed hard disk media' }
-
-            if ($drive) {
-                Write-Host "Iniciando la desfragmentación..."
-                
-                defrag.exe C: /U /V /O
-                
-                Write-Host "Desfragmentación completada."
-            } else {
-                Write-Host "C: no es una unidad. No se realizará la desfragmentación."
-            }
+            Write-Info "Detectando tipo de disco..."
+			$type = Get-DriveType "C"
+			switch ($type) {
+				"SSD" {
+					Write-Warn "SSD detectado. Ejecutando TRIM..."
+					Optimize-Volume -DriveLetter C -ReTrim -Verbose
+					Write-Ok "TRIM completado."
+				}
+				"HDD" {
+					Write-Info "HDD detectado. Desfragmentando..."
+					Optimize-Volume -DriveLetter C -Defrag -Verbose
+					Write-Ok "Desfragmentación completada."
+				}
+				default {
+					Write-Warn "Tipo de disco no determinado ($type). Ejecutando optimización genérica..."
+					Optimize-Volume -DriveLetter C -Verbose
+				}
+    }
+    Read-Host "`nPresiona Enter para cerrar"
         }
         2 {
             Write-Host "Ejecutando sfc /scannow para corregir errores en el sistema..."
             sfc /scannow
         }
 		3 {
-            Write-Host "Ejecutando DISM /Online /Cleanup-Image /RestoreHealth para corregir errores en el sistema..."
-            DISM /Online /Cleanup-Image /RestoreHealth
+            Write-Info "Paso 1/3 — CheckHealth..."
+		    DISM /Online /Cleanup-Image /CheckHealth
+		
+		    Write-Info "Paso 2/3 — ScanHealth..."
+		    DISM /Online /Cleanup-Image /ScanHealth
+		
+		    Write-Info "Paso 3/3 — RestoreHealth..."
+		    DISM /Online /Cleanup-Image /RestoreHealth
         }
         4 {
             Write-Host "Saliendo del programa."
